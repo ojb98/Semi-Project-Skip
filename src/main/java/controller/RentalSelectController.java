6056da55933334473239dc0084e0ada2439b77d4
sangyeop <kio4545@naver.com>
2025-03-21 17:11:36 +0900

    렌탈샵 json 넘기기 완

--- src/main/java/controller/RentalSelectController.java
+++ src/main/java/controller/RentalSelectController.java
@@ -1,10 +1,11 @@
 package controller;
 
 import dao.RentalShopDao;
+import dao.RentalshopReviewDao;
 import dao.SkiDao;
 import dao.SkiReviewDao;
 import dto.RentalShopDTO;
-import dto.ResortReviewDTO;
+import dto.RentalshopReviewDTO;
 import dto.SkiReviewDTO;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -19,8 +20,9 @@ import java.util.List;
 public class RentalSelectController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	System.out.println(req.getParameter("ski_id"));
+    	System.out.println(req.getParameter("rentalshop_id"));
     	
-    	int ski_id = Integer.parseInt(req.getParameter("ski_id"));
     	String sortType = req.getParameter("sort");
     	
         RentalShopDao rentalShopDao = new RentalShopDao();
@@ -28,8 +30,34 @@ public class RentalSelectController extends HttpServlet {
         System.out.println(req.getParameter("isRentalOrSki"));
         String isRentalOrSki = req.getParameter("isRentalOrSki");
         if (isRentalOrSki != null && isRentalOrSki.equalsIgnoreCase("RENTAL")) {
+        	int rentalshop_id = Integer.parseInt(req.getParameter("rentalshop_id"));
+        	
             req.setAttribute("dto", rentalShopDao.getRentalShop(Integer.parseInt(req.getParameter("rentalshop_id"))));
+            
+            RentalshopReviewDao rentalshopReviewDao = new RentalshopReviewDao();
+            List<RentalshopReviewDTO> rentalshopReviewList = rentalshopReviewDao.reviewList(rentalshop_id);
+            
+            req.setAttribute("reviewList", rentalshopReviewList);
+            
+            //총 리뷰 개수
+      		int rentalshopReviewCount = rentalshopReviewList.size();
+      		
+      		//평균 평점 계산
+      		double totalRating = 0;
+      		for(RentalshopReviewDTO review : rentalshopReviewList) {
+      			totalRating += review.getRating();
+      		}
+      		double avgRating = (rentalshopReviewList.size() > 0) ? totalRating / rentalshopReviewList.size() : 0;
+            
+      		req.setAttribute("rentalshop_id", rentalshop_id);
+      		req.setAttribute("reviewList", rentalshopReviewList);
+      		req.setAttribute("reviewCount", rentalshopReviewCount);
+    		req.setAttribute("avgRating",Math.round(avgRating * 100) / 100.0);
+    		req.setAttribute("rentalshopReviewList", rentalshopReviewList);
+      		
         } else if (isRentalOrSki != null && isRentalOrSki.equalsIgnoreCase("SKI")) {
+        	int ski_id = Integer.parseInt(req.getParameter("ski_id"));
+        	
             req.setAttribute("dto", skiDao.getSki(Integer.parseInt(req.getParameter("ski_id"))));
             
             
@@ -52,7 +80,7 @@ public class RentalSelectController extends HttpServlet {
       		req.setAttribute("reviewList", skiReviewList);
       		req.setAttribute("reviewCount", skiReviewCount);
     		req.setAttribute("avgRating",Math.round(avgRating * 100) / 100.0);
-    		req.setAttribute("resortReviewList", skiReviewList);
+    		req.setAttribute("skiReviewList", skiReviewList);
         }
         
         req.setAttribute("isRentalOrSki", isRentalOrSki);
